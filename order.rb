8486698d74ff88d845592600452480c971ee4d73
libbydelbarrio <libbydelbarrio@yahoo.com>
2017-07-27 10:22:27 -0500

    add method to add pizza to order

--- order.rb
+++ order.rb
@@ -2,6 +2,10 @@ class Order
   def initialize(customer, is_delivery, pizzas)
     @customer = customer
     @is_delivery = is_delivery
-    @pizzas = pizzas
+    @pizzas = []
+  end
+
+  def add_pizza(pizza)
+    @pizzas << pizzas
   end
 end
